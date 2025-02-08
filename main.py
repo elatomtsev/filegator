00821bf6a46832b9d09a9735fcf26d7b492fd205
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-02-09 00:06:43 +0300

    одиночный клик по элементам списка заменён на двойной; теперь открываются и файлы

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import Qt, QSize
 
 import os
-back = ['..']
+
+back = [".."]
+
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('btc mainer')
+        self.setWindowTitle("btc mainer")
         self.setFixedSize(QSize(600, 400))
 
-        self.line_edit = QLineEdit(os.getcwd() + '\\')
+        # Записываем в строку поиска путь к текущей директории
+        self.line_edit = QLineEdit(os.getcwd() + "\\")
         self.list_box = QListWidget()
+        # Добавляем директории и файлы, расположенные в текущей директории
         self.list_box.addItems(back + sorted(os.listdir()))
 
         layout = QVBoxLayout()
@@ -24,22 +28,28 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(widget)
 
-        self.list_box.itemClicked.connect(self.add_url_dir)
+        # При двойном клике по каталогу или файлу открываем его
+        self.list_box.itemDoubleClicked.connect(self.add_url_dir)
+        # При изменение строки поиска обновляем текущий список файлов и каталогов
         self.line_edit.textChanged.connect(self.line_edit_changed)
-
 
     def line_edit_changed(self):
         self.list_box.clear()
         self.list_box.addItems(back + os.listdir(self.line_edit.text()))
 
     def add_url_dir(self, e):
-        if '.' not in e.text():
-            self.line_edit.setText(self.line_edit.text() + e.text() + '\\')
-        elif '..' == e.text():
-            self.line_edit.setText('\\'.join(self.line_edit.text().split('\\')[:-2]) + '\\')
+        # Если это каталог, то обновляем строку поиска с этим каталогом (двигаемся вглубь)
+        if "." not in e.text():
+            self.line_edit.setText(self.line_edit.text() + e.text() + "\\")
+        # Если это "..", то убираем из строки поиска текущий каталог (возвращаемся назада)
+        elif ".." == e.text():
+            self.line_edit.setText("\\".join(self.line_edit.text().split("\\")[:-2]) + "\\")
+        # Если это файл, то открываем его
+        elif "." in e.text():
+            os.startfile(self.line_edit.text() + e.text())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
     window.show()

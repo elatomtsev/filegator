aec76dc82f950cd472493fdc0b554047998cb2ad
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-02-10 21:30:30 +0300

    класс MainWindow переделан: теперь от наследует GUI от ui-файла

--- main.py
+++ main.py
@@ -3,63 +3,54 @@ from PySide6.QtCore import Qt, QSize
 
 import os
 
+from ui_main import Ui_MainWindow
+
 back = [".."]
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle("btc mainer")
-        self.setFixedSize(QSize(600, 400))
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
         # Записываем в строку поиска путь к текущей директории
-        self.line_edit = QLineEdit(os.getcwd() + "\\")
-        self.list_box = QListWidget()
+        self.ui.line_edit.setText(os.getcwd() + "\\")
         # Добавляем директории и файлы, расположенные в текущей директории
-        self.list_box.addItems(back + sorted(os.listdir()))
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.line_edit)
-        layout.addWidget(self.list_box)
-        widget = QWidget()
-        widget.setLayout(layout)
-
-        self.setCentralWidget(widget)
+        self.ui.list_box.addItems(back + sorted(os.listdir()))
 
         # При двойном клике по каталогу или файлу открываем его
-        self.list_box.itemDoubleClicked.connect(self.select_element)
+        self.ui.list_box.itemDoubleClicked.connect(self.select_element)
         # При изменение строки поиска обновляем текущий список файлов и каталогов
-        self.line_edit.textChanged.connect(self.line_edit_changed)
+        self.ui.line_edit.textChanged.connect(self.line_edit_changed)
 
     def line_edit_changed(self):
-        self.list_box.clear()
+        self.ui.list_box.clear()
         # Если строка поиска пуста, то выводим список дисков на ПК
-        if self.line_edit.text() == "":
+        if self.ui.line_edit.text() == "":
             # Удаляем слэш из ссылок, чтоб после переход к след. папке небыло двух слэшей
             list_drives = [drive.strip("\\") for drive in os.listdrives()]
-            self.list_box.addItems(list_drives)
+            self.ui.list_box.addItems(list_drives)
         # Если в строке поиска что-то есть
         else:
-            self.list_box.addItems(back + os.listdir(self.line_edit.text()))
+            self.ui.list_box.addItems(back + os.listdir(self.ui.line_edit.text()))
 
     def select_element(self, e):
         # Если это каталог, то обновляем строку поиска с этим каталогом (двигаемся вглубь)
         if "." not in e.text():
-            self.line_edit.setText(self.line_edit.text() + e.text() + "\\")
+            self.ui.line_edit.setText(self.ui.line_edit.text() + e.text() + "\\")
         # Если это "..", то убираем из строки поиска текущий каталог (возвращаемся назада)
         elif ".." == e.text():
-            new_url = "\\".join(self.line_edit.text().split("\\")[:-2]) + "\\"
+            new_url = "\\".join(self.ui.line_edit.text().split("\\")[:-2]) + "\\"
             # Если юзер находился в корне диска, то выводим список дисков
             if new_url == "\\":
-                self.line_edit.setText("")
+                self.ui.line_edit.setText("")
             # Пользователь не в корне диска
             else:
-                self.line_edit.setText("\\".join(self.line_edit.text().split("\\")[:-2]) + "\\")
+                self.ui.line_edit.setText("\\".join(self.ui.line_edit.text().split("\\")[:-2]) + "\\")
         # Если это файл, то открываем его
         elif "." in e.text():
-            os.startfile(self.line_edit.text() + e.text())
+            os.startfile(self.ui.line_edit.text() + e.text())
 
 
 if __name__ == "__main__":

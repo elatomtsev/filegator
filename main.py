4210a38be57d981c44bbf0ca31e8d874f1099663
elatomtsev <o-elatomtsev-m@yandex.ru>
2025-02-11 00:54:41 +0300

    определение файла или папки изменено с in на функции os: благодаря чему пофикшен баг с открытием скрытых папок

--- main.py
+++ main.py
@@ -15,42 +15,46 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         # Записываем в строку поиска путь к текущей директории
-        self.ui.line_edit.setText(os.getcwd() + "\\")
+        self.ui.left_line_edit.setText(os.getcwd() + "\\")
         # Добавляем директории и файлы, расположенные в текущей директории
-        self.ui.list_box.addItems(back + sorted(os.listdir()))
+        self.ui.left_list_box.addItems(back + sorted(os.listdir()))
 
         # При двойном клике по каталогу или файлу открываем его
-        self.ui.list_box.itemDoubleClicked.connect(self.select_element)
+        self.ui.left_list_box.itemDoubleClicked.connect(self.select_element)
         # При изменение строки поиска обновляем текущий список файлов и каталогов
-        self.ui.line_edit.textChanged.connect(self.line_edit_changed)
+        self.ui.left_line_edit.textChanged.connect(self.left_line_edit_changed)
 
-    def line_edit_changed(self):
-        self.ui.list_box.clear()
+    def left_line_edit_changed(self):
+        self.ui.left_list_box.clear()
+
         # Если строка поиска пуста, то выводим список дисков на ПК
-        if self.ui.line_edit.text() == "":
+        if self.ui.left_line_edit.text() == "":
             # Удаляем слэш из ссылок, чтоб после переход к след. папке небыло двух слэшей
             list_drives = [drive.strip("\\") for drive in os.listdrives()]
-            self.ui.list_box.addItems(list_drives)
+            self.ui.left_list_box.addItems(list_drives)
+
         # Если в строке поиска что-то есть
         else:
-            self.ui.list_box.addItems(back + os.listdir(self.ui.line_edit.text()))
+            self.ui.left_list_box.addItems(back + os.listdir(self.ui.left_line_edit.text()))
 
     def select_element(self, e):
-        # Если это каталог, то обновляем строку поиска с этим каталогом (двигаемся вглубь)
-        if "." not in e.text():
-            self.ui.line_edit.setText(self.ui.line_edit.text() + e.text() + "\\")
         # Если это "..", то убираем из строки поиска текущий каталог (возвращаемся назада)
-        elif ".." == e.text():
-            new_url = "\\".join(self.ui.line_edit.text().split("\\")[:-2]) + "\\"
+        if ".." == e.text():
+            new_url = "\\".join(self.ui.left_line_edit.text().split("\\")[:-2]) + "\\"
             # Если юзер находился в корне диска, то выводим список дисков
             if new_url == "\\":
-                self.ui.line_edit.setText("")
+                self.ui.left_line_edit.setText("")
             # Пользователь не в корне диска
             else:
-                self.ui.line_edit.setText("\\".join(self.ui.line_edit.text().split("\\")[:-2]) + "\\")
+                self.ui.left_line_edit.setText("\\".join(self.ui.left_line_edit.text().split("\\")[:-2]) + "\\")
+
+        # Если это каталог, то обновляем строку поиска с этим каталогом (двигаемся вглубь)
+        elif os.path.isdir(self.ui.left_line_edit.text() + e.text()):
+            self.ui.left_line_edit.setText(self.ui.left_line_edit.text() + e.text() + "\\")
+
         # Если это файл, то открываем его
-        elif "." in e.text():
-            os.startfile(self.ui.line_edit.text() + e.text())
+        elif os.path.isfile(self.ui.left_line_edit.text() + e.text()):
+            os.startfile(self.ui.left_line_edit.text() + e.text())
 
 
 if __name__ == "__main__":
